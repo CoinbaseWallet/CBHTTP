8f4c4df271e015d3287260b2aea00c2045bb3cc2
Andrew Gold <andrewmichaelgold@gmail.com>
2019-09-06 09:57:47 -0700

    Combine the mulitple POST methods into one (#15)

--- android/http/src/main/java/com/coinbase/wallet/http/extensions/Response+HTTP.kt
+++ android/http/src/main/java/com/coinbase/wallet/http/extensions/Response+HTTP.kt
@@ -4,13 +4,18 @@ import com.coinbase.wallet.core.util.JSON
 import com.coinbase.wallet.http.exceptions.HTTPException
 import com.coinbase.wallet.http.models.HTTPResponse
 import okhttp3.Response
+import timber.log.Timber
 
 /**
  * Convert an okhttp response to a CBHTTP response
  */
 inline fun <reified T : Any> Response.asHTTPResponse(): HTTPResponse<T> {
-    val json = body()?.string() ?: throw HTTPException.UnableToDeserialize
-    val result = JSON.fromJsonString<T>(json) ?: throw HTTPException.UnableToDeserialize
+    val result = if (T::class.java == ByteArray::class.java) {
+        (body()?.bytes() ?: ByteArray(0)) as T
+    } else {
+        val json = body()?.string() ?: throw HTTPException.UnableToDeserialize
+        JSON.fromJsonString<T>(json) ?: throw HTTPException.UnableToDeserialize
+    }
 
     val rspHeaders = headers()
     val headersMap = (0 until rspHeaders.size())
